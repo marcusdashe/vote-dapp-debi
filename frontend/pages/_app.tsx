9c707d88881054930f35d162e3a4776c206a0282
Marcus Dashe <marcussdashe.developer@gmail.com>
2023-04-17 09:17:45 -0700

    configure it with Hardhat and deployed to Sepolia Testnet

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,60 +1,59 @@
-import '../styles/globals.css';
-import '@rainbow-me/rainbowkit/styles.css';
-import type { AppProps } from 'next/app';
-import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
-import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { hardhat, polygonMumbai } from 'wagmi/chains';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
-import Nav from '../components/Nav'
-import Footer from '../components/Footer'
-import Alert from '../components/Alert'
-import { Toaster } from 'react-hot-toast';
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import type { AppProps } from "next/app";
+import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { hardhat, polygonMumbai, sepolia } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+import Alert from "../components/Alert";
+import { Toaster } from "react-hot-toast";
 
-const toastOptions = {  
+const toastOptions = {
   duration: 3000,
-  position: 'top-center',
+  position: "top-center",
   style: {
-    padding: '8px 8px 8px 16px',
-    color: '#FFFFFF',
-    borderRadius: '4px',
+    padding: "8px 8px 8px 16px",
+    color: "#FFFFFF",
+    borderRadius: "4px",
   },
   success: {
     style: {
-      background: '#039855',
+      background: "#039855",
     },
     iconTheme: {
-      primary: '#fff',
-      secondary: '#039855',
+      primary: "#fff",
+      secondary: "#039855",
     },
   },
   error: {
     style: {
-      background: '#D92D20',
+      background: "#D92D20",
     },
     iconTheme: {
-      primary: '#fff',
-      secondary: '#D92D20',
+      primary: "#fff",
+      secondary: "#D92D20",
     },
   },
-}
+};
 
 const { chains, provider, webSocketProvider } = configureChains(
-  [
-    polygonMumbai
-  ],
+  [sepolia],
   [
     alchemyProvider({
       // This is Alchemy's default API key.
       // You can get your own at https://dashboard.alchemyapi.io
-      apiKey: 'IJtnJ4HYZXb0fXRm8grPnrCFGnyL7Z4X',
+      // apiKey: "IJtnJ4HYZXb0fXRm8grPnrCFGnyL7Z4X",
+      apiKey: "mcjhcWL8cLXfWOlHHO3ofP3hFPBXC5Wq",
     }),
     publicProvider(),
   ]
 );
 
 const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
+  appName: "RainbowKit App",
   chains,
 });
 
@@ -66,14 +65,12 @@ const wagmiClient = createClient({
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
-
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
-        <div  className='h-screen bg-vote-50 pb-12 relative'>
+        <div className="h-screen bg-vote-50 pb-12 relative">
           <Nav />
-          <div className='w-11/12 md:w-6/12 mx-auto py-12'>
-            
+          <div className="w-11/12 md:w-6/12 mx-auto py-12">
             <Component {...pageProps} />
 
             <Toaster toastOptions={toastOptions} />
